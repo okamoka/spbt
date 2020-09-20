4bfe62baccec245562469aa1e4978e40ab6fb9c9
okamasa <july.7th.1977@gmail.com>
2020-09-21 08:28:24 +0900

    画面レイアウト共通化　bootstrap 導入　ちょっと全体的に古いのみなおさないと

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,5 @@
 package com.example.demo;
 
-import java.util.Optional;
-
 import com.example.demo.domain.jpasample.Customer;
 import com.example.demo.domain.jpasample.FileLob;
 import com.example.demo.usecase.repository.CustomerRepository;
